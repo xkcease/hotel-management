2f5e0b95f84a26c2a60cb5038b228368cd9e33be
xkcease <xkcease@163.com>
2021-01-09 17:41:24 +0800

    feat: Navbar and Sidebar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="navbar">
+        <h2>hotel management</h2>
+        <div>
+            <el-badge class="navbar__bell" :is-dot="true">
+                <i class="el-icon-bell"></i>
+            </el-badge>
+            <el-dropdown trigger="click">
+                <span>
+                    {{ 11 }}
+                    <i class="el-icon-arrow-down"></i>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-item>个人信息</el-dropdown-item>
+                    <el-dropdown-item>安全中心</el-dropdown-item>
+                    <el-dropdown-item>退出</el-dropdown-item>
+                </template>
+            </el-dropdown>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    setup() {},
+};
+</script>
+
+<style lang="scss">
+.navbar {
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .navbar__bell {
+        margin-right: 12px;
+    }
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,52 @@
+<template>
+    <el-scrollbar class="sidebar">
+        <el-menu
+            mode="vertical"
+            background-color="#1f2d3d"
+            text-color="#bfcbd9"
+        >
+            <el-menu-item index="1">
+                <h2 class="sidebar__item">
+                    <svg-icon icon-class="home"></svg-icon>
+                    主页
+                </h2>
+            </el-menu-item>
+            <el-submenu index="2">
+                <template #title>喵</template>
+                <el-menu-item index="2-1">1</el-menu-item>
+                <el-menu-item index="2-2">2</el-menu-item>
+                <el-menu-item index="2-3">3</el-menu-item>
+                <el-menu-item index="2-4">4</el-menu-item>
+            </el-submenu>
+        </el-menu>
+    </el-scrollbar>
+</template>
+
+<script>
+import { ref } from 'vue';
+import SvgIcon from './SvgIcon.vue';
+
+export default {
+    components: { SvgIcon },
+    name: 'Sidebar',
+    setup() {
+        const activeIndex = ref('2-1');
+
+        return {
+            activeIndex,
+        };
+    },
+};
+</script>
+
+<style lang="scss">
+.sidebar {
+    .sidebar__item {
+        font-size: 18px;
+    }
+
+    .el-menu {
+        border-right: 0;
+    }
+}
+</style>
