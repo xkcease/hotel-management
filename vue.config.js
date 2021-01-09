62035dc9708a7d784d5dcf2f122f4536c3fd1b7d
xkcease <xkcease@163.com>
2021-01-09 17:45:26 +0800

    build: add style-resources-loader and svg-sprite-loader

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,33 @@
+const path = require('path');
+
 module.exports = {
     devServer: {
         port: 9090,
+    },
+    pluginOptions: {
+        'style-resources-loader': {
+            preProcessor: 'scss',
+            patterns: [path.resolve(__dirname, './src/assets/scss/index.scss')],
+        },
+    },
+    chainWebpack(config) {
+        config.module
+            .rule('svg')
+            .exclude.add(path.join(__dirname, 'src/icons'))
+            .end();
+        //     .uses.clear()
+        // config.module.rules.delete('svg');
+
+        config.module
+            .rule('icons')
+            .test(/\.svg$/)
+            .include.add(path.join(__dirname, 'src/icons'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]',
+            })
+            .end();
     },
 };
