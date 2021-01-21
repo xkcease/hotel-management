7c7f05cb2a038f9c0b6dcf3d54c1594213435d34
xkcease <xkcease@163.com>
2021-01-21 18:18:57 +0800

    feat(commponents): Navbar and Sidebar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,23 @@
 <template>
     <div class="navbar">
-        <h2>hotel management</h2>
+        <div>
+            <svg-icon icon-class="hotel" class="navbar__brand"></svg-icon>
+            <h2 class="navbar__title">hotel management</h2>
+        </div>
         <div>
             <el-badge class="navbar__bell" :is-dot="true">
                 <i class="el-icon-bell"></i>
             </el-badge>
             <el-dropdown trigger="click">
-                <span>
-                    {{ 11 }}
+                <span class="navbar__name">
+                    {{ username }}
                     <i class="el-icon-arrow-down"></i>
                 </span>
                 <template #dropdown>
-                    <el-dropdown-item>个人信息</el-dropdown-item>
-                    <el-dropdown-item>安全中心</el-dropdown-item>
-                    <el-dropdown-item>退出</el-dropdown-item>
+                    <el-dropdown-item @click="toModifyPassword">
+                        修改密码
+                    </el-dropdown-item>
+                    <el-dropdown-item @click="logout">退出</el-dropdown-item>
                 </template>
             </el-dropdown>
         </div>
@@ -21,9 +25,31 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
+import SvgIcon from './SvgIcon.vue';
+
 export default {
+    components: { SvgIcon },
     name: 'Navbar',
-    setup() {},
+    setup() {
+        const username = sessionStorage.getItem('username');
+        const router = useRouter();
+
+        const toModifyPassword = () => {
+            router.push({ name: 'ModifyPassword' });
+        };
+
+        const logout = () => {
+            sessionStorage.clear();
+            router.push({ name: 'Login' });
+        };
+
+        return {
+            username,
+            toModifyPassword,
+            logout,
+        };
+    },
 };
 </script>
 
@@ -34,8 +60,26 @@ export default {
     justify-content: space-between;
     align-items: center;
 
+    .navbar__brand {
+        width: 30px;
+        height: 30px;
+    }
+
+    .navbar__title {
+        display: inline-block;
+        font-size: 21px;
+        color: $theme-color;
+        font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+            Microsoft YaHei, Arial, sans-serif;
+        padding-left: 12px;
+    }
+
     .navbar__bell {
         margin-right: 12px;
+    }
+
+    .navbar__name {
+        cursor: pointer;
     }
 }
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,6 +4,7 @@
             mode="vertical"
             background-color="#1f2d3d"
             text-color="#bfcbd9"
+            default-active="1"
         >
             <el-menu-item index="1">
                 <h2 class="sidebar__item">
@@ -12,11 +13,27 @@
                 </h2>
             </el-menu-item>
             <el-submenu index="2">
-                <template #title>喵</template>
-                <el-menu-item index="2-1">1</el-menu-item>
-                <el-menu-item index="2-2">2</el-menu-item>
-                <el-menu-item index="2-3">3</el-menu-item>
-                <el-menu-item index="2-4">4</el-menu-item>
+                <template #title>
+                    <h2 class="sidebar__item">订单管理</h2>
+                </template>
+                <el-menu-item index="2-1">未入住</el-menu-item>
+                <el-menu-item index="2-2">已入住</el-menu-item>
+                <el-menu-item index="2-3">已退房</el-menu-item>
+                <el-menu-item index="2-4">全部</el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+                <template #title
+                    ><h2 class="sidebar__item">房间管理</h2></template
+                >
+                <el-menu-item index="3-1">查看房间</el-menu-item>
+                <el-menu-item index="3-2">添加新房间</el-menu-item>
+            </el-submenu>
+            <el-submenu index="4">
+                <template #title
+                    ><h2 class="sidebar__item">账号管理</h2></template
+                >
+                <el-menu-item index="4-1">查看账号</el-menu-item>
+                <el-menu-item index="4-2">注册新账号</el-menu-item>
             </el-submenu>
         </el-menu>
     </el-scrollbar>
@@ -42,7 +59,7 @@ export default {
 <style lang="scss">
 .sidebar {
     .sidebar__item {
-        font-size: 18px;
+        font-size: 16px;
     }
 
     .el-menu {
