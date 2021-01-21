b164058ff4f595a1b1188c551973703f868e5ff3
xkcease <xkcease@163.com>
2021-01-21 18:22:08 +0800

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,20 @@
 import { createStore } from 'vuex';
 
-export default createStore({
-    state: {},
-    mutations: {},
+const store = createStore({
+    state: {
+        permission: -1,
+        loading: null,
+    },
+    mutations: {
+        setPermission(state, payload) {
+            state.permission = payload.permission;
+        },
+        setLoading(state, payload) {
+            state.loading = payload.loading;
+        }
+    },
     actions: {},
     modules: {},
 });
+
+export default store;
