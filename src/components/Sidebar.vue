3a375248f103b24faeb34dac0982c258d37704a3
xkcease <xkcease@163.com>
2021-01-25 02:38:34 +0800

    feat: update Sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,8 +5,9 @@
             background-color="#1f2d3d"
             text-color="#bfcbd9"
             default-active="1"
+            :router="true"
         >
-            <el-menu-item index="1">
+            <el-menu-item index="home">
                 <h2 class="sidebar__item">
                     <svg-icon icon-class="home"></svg-icon>
                     主页
@@ -21,19 +22,19 @@
                 <el-menu-item index="2-3">已退房</el-menu-item>
                 <el-menu-item index="2-4">全部</el-menu-item>
             </el-submenu>
-            <el-submenu index="3">
+            <el-submenu index="3" v-if="permission <= 1">
                 <template #title
                     ><h2 class="sidebar__item">房间管理</h2></template
                 >
-                <el-menu-item index="3-1">查看房间</el-menu-item>
-                <el-menu-item index="3-2">添加新房间</el-menu-item>
+                <el-menu-item index="roomList">查看房间</el-menu-item>
+                <el-menu-item index="addRoom">添加新房间</el-menu-item>
             </el-submenu>
-            <el-submenu index="4">
+            <el-submenu index="4" v-if="permission === 0">
                 <template #title
                     ><h2 class="sidebar__item">账号管理</h2></template
                 >
-                <el-menu-item index="4-1">查看账号</el-menu-item>
-                <el-menu-item index="4-2">注册新账号</el-menu-item>
+                <el-menu-item index="userList">查看用户</el-menu-item>
+                <el-menu-item index="addUser">注册新用户</el-menu-item>
             </el-submenu>
         </el-menu>
     </el-scrollbar>
@@ -41,16 +42,22 @@
 
 <script>
 import { ref } from 'vue';
+import { useStore } from 'vuex';
 import SvgIcon from './SvgIcon.vue';
 
 export default {
     components: { SvgIcon },
     name: 'Sidebar',
     setup() {
-        const activeIndex = ref('2-1');
+        const store = useStore();
+
+        const activeIndex = ref('home');
+
+        const permission = store.state.permission;
 
         return {
             activeIndex,
+            permission,
         };
     },
 };
