5b4fcaec90ba40a80a8a074054ef4eb994c48400
xkcease <xkcease@163.com>
2021-01-28 19:03:49 +0800

    feat(utils): add roomRequest

---
+++ src/utils/roomRequest.js
@@ -0,0 +1,34 @@
+import { axiosGet, axiosPost, axiosPostFile } from './http';
+
+function addRoomRequest(form) {
+    return axiosPost('/addRoom', form);
+}
+
+function uploadImgRequest(file) {
+    return axiosPostFile('/uploadImg', file);
+}
+
+function getRoomsRequest() {
+    return axiosGet('/getRooms');
+}
+
+function getRoomInfoRequest(number) {
+    return axiosGet('/getRoomInfo', { number });
+}
+
+function updateRoomRequest(form) {
+    return axiosPost('/updateRoom', form);
+}
+
+function deleteRoomRequest(number) {
+    return axiosPost('/deleteRoom', { number });
+}
+
+export {
+    addRoomRequest,
+    uploadImgRequest,
+    getRoomsRequest,
+    getRoomInfoRequest,
+    updateRoomRequest,
+    deleteRoomRequest,
+}
