c00cdf557a06c1d264aa6bd4d67d350a0ffaa6e1
xkcease <xkcease@163.com>
2021-02-18 15:44:52 +0800

    feat: update Sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -33,7 +33,14 @@
                     修改价格
                 </el-menu-item>
             </el-submenu>
-            <el-submenu index="4" v-if="permission === 0">
+            <el-submenu index="4" v-if="permission <= 1">
+                <template #title>
+                    <h2 class="sidebar__item">小程序</h2>
+                </template>
+                <el-menu-item index="hotelIntro">酒店简介</el-menu-item>
+                <el-menu-item index="roomIntro"> 房间描述 </el-menu-item>
+            </el-submenu>
+            <el-submenu index="5" v-if="permission === 0">
                 <template #title
                     ><h2 class="sidebar__item">账号管理</h2></template
                 >
