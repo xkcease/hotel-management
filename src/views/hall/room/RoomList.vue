19012568a354cc242fe45add9216a3429e79d0bb
xkcease <xkcease@163.com>
2021-03-21 23:03:28 +0800

    feat(views): update RoomList

--- src/views/hall/room/RoomList.vue
+++ src/views/hall/room/RoomList.vue
@@ -1,123 +1,128 @@
 <template>
     <div class="room-list">
-        <el-table :data="list" :default-sort="defaultSort">
-            <el-table-column label="图片">
-                <template #default="scope">
-                    <el-image
-                        class="room-list__img"
-                        v-if="scope.row.img"
-                        :src="scope.row.img"
-                        :preview-src-list="[scope.row.img]"
-                    ></el-image>
-                    <span v-else>无图片</span>
-                </template>
-            </el-table-column>
-            <el-table-column
-                label="房号"
-                prop="number"
-                sortable
-            ></el-table-column>
-            <el-table-column
-                label="类型"
-                prop="typeText"
-                :sort-method="sortType"
-                sortable
-            ></el-table-column>
-            <el-table-column label="浴室" prop="showerText"></el-table-column>
-            <el-table-column label="电视" prop="tvText"></el-table-column>
-            <el-table-column label="其他" prop="extra"></el-table-column>
-            <el-table-column>
-                <template #header>
-                    <el-checkbox v-model="stateChecked" @change="showAvailable">
-                        状态
-                    </el-checkbox>
-                </template>
-                <template #default="scope">
-                    <el-tag
-                        :type="scope.row.state ? 'danger' : 'success'"
-                        effect="dark"
-                    >
-                        {{ scope.row.stateText }}
-                    </el-tag>
-                </template>
-            </el-table-column>
-            <el-table-column align="right">
-                <template #header>
-                    <el-input
-                        size="small"
-                        placeholder="输入关键字搜索"
-                        v-model="keyword"
-                        @input="search"
-                    />
-                </template>
-                <template #default="scope">
-                    <div class="l-flex l-flex__justify--between">
-                        <el-button
-                            size="mini"
-                            type="primary"
-                            @click="toCheckIn(scope.row)"
-                            :disabled="Boolean(scope.row.state)"
+        <SearchFilter
+            v-model="conditions"
+            :searchOptions="searchOptions"
+            type
+            state
+            shower
+            tv
+            search
+        />
+        <div class="room-list__table">
+            <el-table :data="list" :default-sort="defaultSort" stripe border>
+                <el-table-column
+                    label="房号"
+                    prop="number"
+                    align="center"
+                    sortable
+                ></el-table-column>
+                <el-table-column
+                    label="类型"
+                    prop="typeText"
+                    align="center"
+                    :sort-method="sortType"
+                    sortable
+                ></el-table-column>
+                <el-table-column
+                    label="浴室"
+                    prop="showerText"
+                    align="center"
+                ></el-table-column>
+                <el-table-column
+                    label="电视"
+                    prop="tvText"
+                    align="center"
+                ></el-table-column>
+                <el-table-column
+                    label="其他"
+                    prop="extra"
+                    align="center"
+                ></el-table-column>
+                <el-table-column label="房间状态" align="center">
+                    <template #default="scope">
+                        <el-tag
+                            :type="scope.row.state ? 'danger' : 'success'"
+                            effect="dark"
                         >
-                            办理入住
-                        </el-button>
-                        <el-dropdown
-                            v-if="permission <= 1"
-                            size="small"
-                            trigger="click"
-                        >
-                            <el-button size="mini">
-                                <i class="el-icon-arrow-down"></i>
+                            {{ scope.row.stateText }}
+                        </el-tag>
+                    </template>
+                </el-table-column>
+                <el-table-column align="center" label="操作" width="200">
+                    <template #default="scope">
+                        <div class="l-flex l-flex__justify--between">
+                            <el-button
+                                size="mini"
+                                type="primary"
+                                @click="toCheckIn(scope.row)"
+                                :disabled="Boolean(scope.row.state)"
+                            >
+                                办理入住
                             </el-button>
-                            <template #dropdown>
-                                <el-dropdown-menu>
-                                    <el-dropdown-item>
-                                        <el-button
-                                            size="mini"
-                                            style="margin-bottom: 6px"
-                                            @click="toModifyRoom(scope.row)"
-                                        >
-                                            更改
-                                        </el-button>
-                                    </el-dropdown-item>
-                                    <el-dropdown-item>
-                                        <el-popconfirm
-                                            icon="el-icon-info"
-                                            iconColor="red"
-                                            confirmButtonText="是"
-                                            cancelButtonText="否"
-                                            title="确定要删除吗？"
-                                            @confirm="deleteRoom(scope.row)"
-                                        >
-                                            <template #reference>
-                                                <el-button
-                                                    size="mini"
-                                                    type="danger"
-                                                >
-                                                    删除
-                                                </el-button>
-                                            </template>
-                                        </el-popconfirm>
-                                    </el-dropdown-item>
-                                </el-dropdown-menu>
-                            </template>
-                        </el-dropdown>
-                    </div>
-                </template>
-            </el-table-column>
-        </el-table>
+                            <el-dropdown
+                                v-if="permission <= 1"
+                                size="small"
+                                trigger="click"
+                            >
+                                <el-button size="mini">
+                                    <i class="el-icon-arrow-down"></i>
+                                </el-button>
+                                <template #dropdown>
+                                    <el-dropdown-menu>
+                                        <el-dropdown-item>
+                                            <el-button
+                                                size="mini"
+                                                style="margin-bottom: 6px"
+                                                @click="toModifyRoom(scope.row)"
+                                            >
+                                                更改
+                                            </el-button>
+                                        </el-dropdown-item>
+                                        <el-dropdown-item>
+                                            <el-popconfirm
+                                                icon="el-icon-info"
+                                                iconColor="red"
+                                                confirmButtonText="是"
+                                                cancelButtonText="否"
+                                                title="确定要删除吗？"
+                                                @confirm="deleteRoom(scope.row)"
+                                            >
+                                                <template #reference>
+                                                    <el-button
+                                                        size="mini"
+                                                        type="danger"
+                                                    >
+                                                        删除
+                                                    </el-button>
+                                                </template>
+                                            </el-popconfirm>
+                                        </el-dropdown-item>
+                                    </el-dropdown-menu>
+                                </template>
+                            </el-dropdown>
+                        </div>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
     </div>
 </template>
 
 <script>
-import { reactive, toRefs, ref, watchEffect, inject } from 'vue';
+import { reactive, toRefs, watchEffect, watch, inject } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import { useStore } from 'vuex';
 import { ElMessage } from 'element-plus';
 import loading from '@/utils/loading';
 import { getRoomsRequest, deleteRoomRequest } from '@/utils/roomRequest';
+import SearchFilter from '@/components/SearchFilter';
 
 export default {
     name: 'RoomList',
+    components: {
+        SearchFilter,
+    },
     setup() {
         const router = useRouter();
         const route = useRoute();
@@ -125,6 +130,64 @@ export default {
         const permission = store.state.permission;
 
         const reload = inject('reload');
+
+        const searchOptions = [
+            {
+                label: '房号',
+                value: 'number',
+            },
+        ];
+
+        const conditionsData = reactive({
+            conditions: [],
+        });
+
+        // 查询
+        const search = () => {
+            let list = tableData.origin;
+
+            for (let condition of conditionsData.conditions) {
+                if (condition.key === 'type') {
+                    list = list.filter((order) => {
+                        return order.type === condition.value;
+                    });
+                }
+
+                if (condition.key === 'state') {
+                    list = list.filter((order) => {
+                        return order.state === condition.value;
+                    });
+                }
+
+                if (condition.key === 'shower') {
+                    list = list.filter((order) => {
+                        return order.shower === condition.value;
+                    });
+                }
+
+                if (condition.key === 'tv') {
+                    list = list.filter((order) => {
+                        return order.tv === condition.value;
+                    });
+                }
+
+                if (condition.key === 'search') {
+                    const keywordReg = new RegExp(condition.value);
+
+                    list = list.filter((order) => {
+                        return keywordReg.test(order[condition.searchKey]);
+                    });
+                }
+            }
+            tableData.list = list;
+        };
+
+        watch(
+            () => conditionsData.conditions,
+            () => {
+                search();
+            }
+        );
 
         const tableData = reactive({
             origin: [],
@@ -203,61 +266,16 @@ export default {
                 });
         };
 
-        const stateChecked = ref(false);
-        const showAvailable = () => {
-            if (stateChecked.value) {
-                tableData.list = tableData.list.filter((room) => {
-                    return room.state === 0;
-                });
-            } else {
-                tableData.list = tableData.search;
-            }
-        };
-
-        // 搜索
-        let keyword = ref('');
-        const search = () => {
-            const numberReg = /^[0-9a-zA-Z]+$/;
-            const typeTextReg = /^[\u4e00-\u9fa5]+$/;
-            const keywordReg = new RegExp(keyword.value);
-
-            let searchArray = [];
-            let searchKey = '';
-
-            if (numberReg.test(keyword.value)) {
-                searchKey = 'number';
-            } else if (typeTextReg.test(keyword.value)) {
-                searchKey = 'typeText';
-            }
-
-            for (let room of tableData.origin) {
-                if (keywordReg.test(room[searchKey])) {
-                    searchArray.push(room);
-                }
-            }
-
-            if (stateChecked.value) {
-                searchArray = searchArray.filter((room) => {
-                    return room.state === 0;
-                });
-            }
-
-            tableData.list = searchArray;
-            tableData.search = searchArray;
-        };
-
         return {
             permission,
+            searchOptions,
+            ...toRefs(conditionsData),
             ...toRefs(tableData),
             sortType,
             defaultSort,
             toModifyRoom,
             toCheckIn,
             deleteRoom,
-            stateChecked,
-            showAvailable,
-            keyword,
-            search,
         };
     },
 };
@@ -266,12 +284,11 @@ export default {
 <style lang="scss">
 .room-list {
     margin: 30px 10px;
-    border: 1px solid #ebeef5;
     padding: 0 20px;
 
-    .room-list__img {
-        width: 100px;
-        height: 100px;
+    .room-list__table {
+        border: 1px solid #ebeef5;
+        margin-top: 10px;
     }
 }
 </style>
