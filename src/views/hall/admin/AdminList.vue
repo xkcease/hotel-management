c9b63807e7930356b4ac49a387f9f0c421e26e25
xkcease <xkcease@163.com>
2021-03-21 22:18:55 +0800

    feat(views): update AdminList

--- src/views/hall/admin/AdminList.vue
+++ src/views/hall/admin/AdminList.vue
@@ -1,74 +1,87 @@
 <template>
     <div class="admin-list">
-        <el-table :data="list">
-            <el-table-column label="Id" prop="aid" sortable></el-table-column>
-            <el-table-column
-                label="用户名"
-                prop="username"
-                sortable
-            ></el-table-column>
-            <el-table-column
-                label="权限"
-                prop="text"
-                :sort-method="sortPermission"
-                sortable
-            ></el-table-column>
-            <el-table-column align="right">
-                <template #header>
-                    <el-input
-                        size="small"
-                        placeholder="输入关键字搜索"
-                        v-model="keyword"
-                        @input="search"
-                    />
-                </template>
-                <template #default="scope">
-                    <div class="l-flex l-flex__justify--between">
-                        <el-dropdown size="small" trigger="click">
-                            <el-button size="mini"> 修改权限 </el-button>
-                            <template #dropdown>
-                                <el-dropdown-menu>
-                                    <el-dropdown-item
-                                        @click="modifyPermission(scope.row, 2)"
-                                    >
-                                        普通管理员
-                                    </el-dropdown-item>
-                                    <el-dropdown-item
-                                        @click="modifyPermission(scope.row, 1)"
-                                    >
-                                        高级管理员
-                                    </el-dropdown-item>
-                                    <el-dropdown-item
-                                        @click="modifyPermission(scope.row, 0)"
-                                    >
-                                        超级管理员
-                                    </el-dropdown-item>
-                                </el-dropdown-menu>
-                            </template>
-                        </el-dropdown>
-                        <el-popconfirm
-                            icon="el-icon-info"
-                            iconColor="red"
-                            confirmButtonText="是"
-                            cancelButtonText="否"
-                            title="确定要删除吗？"
-                            @confirm="deleteAdmin(scope.row)"
-                        >
-                            <template #reference>
-                                <el-button size="mini" type="danger">
-                                    删除
-                                </el-button>
-                            </template>
-                        </el-popconfirm>
-                    </div>
-                </template>
-            </el-table-column>
-        </el-table>
+        <SearchFilter
+            v-model="conditions"
+            :searchOptions="searchOptions"
+            permission
+            search
+        />
+        <div class="admin-list__table">
+            <el-table :data="list" stripe border>
+                <el-table-column
+                    label="Id"
+                    prop="aid"
+                    align="center"
+                    sortable
+                ></el-table-column>
+                <el-table-column
+                    label="用户名"
+                    prop="username"
+                    align="center"
+                    sortable
+                ></el-table-column>
+                <el-table-column
+                    label="权限"
+                    prop="text"
+                    align="center"
+                    :sort-method="sortPermission"
+                    sortable
+                ></el-table-column>
+                <el-table-column align="center" label="操作" width="300">
+                    <template #default="scope">
+                        <div class="l-flex l-flex__justify--around">
+                            <el-dropdown size="small" trigger="click">
+                                <el-button size="mini"> 修改权限 </el-button>
+                                <template #dropdown>
+                                    <el-dropdown-menu>
+                                        <el-dropdown-item
+                                            @click="
+                                                modifyPermission(scope.row, 2)
+                                            "
+                                        >
+                                            普通管理员
+                                        </el-dropdown-item>
+                                        <el-dropdown-item
+                                            @click="
+                                                modifyPermission(scope.row, 1)
+                                            "
+                                        >
+                                            高级管理员
+                                        </el-dropdown-item>
+                                        <el-dropdown-item
+                                            @click="
+                                                modifyPermission(scope.row, 0)
+                                            "
+                                        >
+                                            超级管理员
+                                        </el-dropdown-item>
+                                    </el-dropdown-menu>
+                                </template>
+                            </el-dropdown>
+                            <el-popconfirm
+                                icon="el-icon-info"
+                                iconColor="red"
+                                confirmButtonText="是"
+                                cancelButtonText="否"
+                                title="确定要删除吗？"
+                                @confirm="deleteAdmin(scope.row)"
+                            >
+                                <template #reference>
+                                    <el-button size="mini" type="danger">
+                                        删除
+                                    </el-button>
+                                </template>
+                            </el-popconfirm>
+                        </div>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
     </div>
 </template>
 
 <script>
-import { reactive, toRefs, ref, inject } from 'vue';
+import { reactive, toRefs, watch, inject } from 'vue';
 import { ElMessage } from 'element-plus';
 import {
     getAdminsRequest,
@@ -76,11 +89,59 @@ import {
     deleteAdminRequest,
 } from '@/utils/adminRequest';
 import loading from '@/utils/loading';
+import SearchFilter from '@/components/SearchFilter';
 
 export default {
     name: 'AdminList',
+    components: {
+        SearchFilter,
+    },
     setup() {
         const reload = inject('reload');
+
+        const searchOptions = [
+            {
+                label: '管理员编号',
+                value: 'aid',
+            },
+            {
+                label: '用户名',
+                value: 'username',
+            },
+        ];
+
+        const conditionsData = reactive({
+            conditions: [],
+        });
+
+        // 查询
+        const search = () => {
+            let list = tableData.origin;
+
+            for (let condition of conditionsData.conditions) {
+                if (condition.key === 'permission') {
+                    list = list.filter((admin) => {
+                        return admin.permission === condition.value;
+                    });
+                }
+
+                if (condition.key === 'search') {
+                    const keywordReg = new RegExp(condition.value);
+
+                    list = list.filter((admin) => {
+                        return keywordReg.test(admin[condition.searchKey]);
+                    });
+                }
+            }
+            tableData.list = list;
+        };
+
+        watch(
+            () => conditionsData.conditions,
+            () => {
+                search();
+            }
+        );
 
         const tableData = reactive({
             origin: [],
@@ -162,41 +223,14 @@ export default {
                 });
         };
 
-        // 搜索
-        let keyword = ref('');
-        const search = () => {
-            const aidReg = /^\d+$/;
-            const usernameReg = /^[0-9a-zA-z]+$/;
-            const textReg = /^[\u4e00-\u9fa5]+$/;
-            const keywordReg = new RegExp(keyword.value);
-
-            const searchArray = [];
-            let searchKey = '';
-
-            if (aidReg.test(keyword.value)) {
-                searchKey = 'aid';
-            } else if (usernameReg.test(keyword.value)) {
-                searchKey = 'username';
-            } else if (textReg.test(keyword.value)) {
-                searchKey = 'text';
-            }
-
-            for (let admin of tableData.origin) {
-                if (keywordReg.test(admin[searchKey])) {
-                    searchArray.push(admin);
-                }
-            }
-
-            tableData.list = searchArray;
-        };
-
         return {
+            ...toRefs(conditionsData),
+            searchOptions,
+            search,
             ...toRefs(tableData),
             sortPermission,
             modifyPermission,
             deleteAdmin,
-            keyword,
-            search,
         };
     },
 };
@@ -205,7 +239,11 @@ export default {
 <style lang="scss">
 .admin-list {
     margin: 30px 50px;
-    border: 1px solid #ebeef5;
     padding: 0 20px;
+
+    .admin-list__table {
+        border: 1px solid #ebeef5;
+        margin-top: 10px;
+    }
 }
 </style>
