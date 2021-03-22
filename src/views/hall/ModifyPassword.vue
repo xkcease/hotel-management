970b345ce1af652d5f08b575d2b3cc6f4010461c
xkcease <xkcease@163.com>
2021-03-22 17:27:15 +0800

    feat(views): update ModifyPassword

--- src/views/hall/ModifyPassword.vue
+++ src/views/hall/ModifyPassword.vue
@@ -1,42 +1,53 @@
 <template>
     <div class="modify-password">
-        <el-form :model="form" :rules="rules" ref="formElem">
-            <p class="form__msg">{{ errorMsg }}</p>
-            <el-form-item prop="idkey">
-                <el-input v-model="form.idkey" placeholder="请输入密保">
-                    <template #prefix>
-                        <i class="el-icon-key form__icon"></i>
-                    </template>
-                </el-input>
-            </el-form-item>
-            <el-form-item prop="password">
-                <el-input
-                    v-model="form.password"
-                    placeholder="请输入密码(长度4-20的数字或字母或下划线)"
-                    show-password
-                >
-                    <template #prefix>
-                        <i class="el-icon-lock form__icon"></i>
-                    </template>
-                </el-input>
-            </el-form-item>
-            <el-form-item prop="againPassword">
-                <el-input
-                    v-model="form.againPassword"
-                    placeholder="请再次输入密码"
-                    show-password
-                >
-                    <template #prefix>
-                        <i class="el-icon-lock form__icon"></i>
-                    </template>
-                </el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button class="form__btn" type="primary" @click="modify">
-                    修改密码
-                </el-button>
-            </el-form-item>
-        </el-form>
+        <div class="form__title">
+            <h2>修改密码</h2>
+        </div>
+        <div class="modify-password__form">
+            <el-form
+                :model="form"
+                :rules="rules"
+                ref="formElem"
+                label-position="right"
+                label-width="80px"
+            >
+                <p class="form__msg">{{ errorMsg }}</p>
+                <el-form-item prop="idkey" label="密保">
+                    <el-input v-model="form.idkey" placeholder="请输入密保">
+                        <template #prefix>
+                            <i class="el-icon-key form__icon"></i>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="password" label="密码">
+                    <el-input
+                        v-model="form.password"
+                        placeholder="请输入密码(长度4-20的数字或字母或下划线)"
+                        show-password
+                    >
+                        <template #prefix>
+                            <i class="el-icon-lock form__icon"></i>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="againPassword" label="确认密码">
+                    <el-input
+                        v-model="form.againPassword"
+                        placeholder="请再次输入密码"
+                        show-password
+                    >
+                        <template #prefix>
+                            <i class="el-icon-lock form__icon"></i>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button class="form__btn" type="primary" @click="modify">
+                        修改密码
+                    </el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
@@ -139,7 +150,11 @@ export default {
 
 <style lang="scss">
 .modify-password {
-    width: 30%;
-    margin: 50px auto;
+    padding: 20px;
+
+    .modify-password__form {
+        width: 50%;
+        margin: 16px;
+    }
 }
 </style>
